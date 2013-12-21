13d72e3bec126d00e4207fc44a133c322dde87b3
raa0121 <raa0121@gmail.com>
2013-12-22 05:17:38 +0900

    first commit

---
+++ neobundler.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/sh
+
+cat > .neobundlerc <<EOF
+set nocompatible
+filetype off
+if has('vim_starting')
+  set runtimepath+=./.neobundle
+  call neobundle#rc(expand('./lib'))
+endif
+source ./NeoBundlefile
+filetype plugin indent on
+EOF
+
+if [ -s .neobundle ] ; then
+  cd .neobundle && git pull --rebase && cd ..
+else
+  git clone git://github.com/Shougo/neobundle.vim.git ./.neobundle
+fi
+
+mkdir -p lib
+if [ $1 = "install" ] ; then
+  vim -u NONE --cmd "so .neobundlerc | NeoBundleInstall | q!"
+elif [ $1 = "update" ] ; then
+  vim -u NONE --cmd "so .neobundlerc | NeoBundleUpdate| q!"
+fi
